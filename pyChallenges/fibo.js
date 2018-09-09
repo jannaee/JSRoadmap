84983ed4ba0230785be20dfca22449817a1b5606
Jannaee <jannaee@gmail.com>
2018-09-08 22:24:45 -0400

    almost have this working

--- pyChallenges/fibo.js
+++ pyChallenges/fibo.js
@@ -2,17 +2,20 @@
 //less than the variable max passed into the function
 
 const arr = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-function solution(max){
 //reduce array to even numbers
-    const evenNums = arr.filter(function (num){return ((num %2)===0)})
-    evenNums.reduce(function (accumulator, currentValue) {
-        return accumulator + currentValue
-    });
-    //building control of data
-    if(max >= evenNums.lastIndexOf()){
+const evenNums = arr.filter(function (num){return ((num %2)===0)})
+let newVal = evenNums.reduce(function (accumulator, currentValue) {
+    return accumulator + currentValue
+});
+
+console.log(newVal[9]);
+//building control of data
+
+function solution (max){
+    if( max >= arr[9]){
         alert(`yes`)
+    } else {
+        alert(`no`);
     }
-
 }
-
 solution(84);
