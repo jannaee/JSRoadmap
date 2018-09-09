88da6fa2290044fe922f3cdf6a30b359e283b61e
Jannaee <jannaee@gmail.com>
2018-09-08 22:03:13 -0400

    stuck! checking in files before it starts to get crazy

--- pyChallenges/fibo.js
+++ pyChallenges/fibo.js
@@ -2,9 +2,17 @@
 //less than the variable max passed into the function
 
 const arr = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-
+function solution(max){
 //reduce array to even numbers
-const evenNums = arr.filter(function (num){return ((num %2)===0)})
-console.log(evenNums);
+    const evenNums = arr.filter(function (num){return ((num %2)===0)})
+    evenNums.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue
+    });
+    //building control of data
+    if(max >= evenNums.lastIndexOf()){
+        alert(`yes`)
+    }
 
+}
 
+solution(84);
