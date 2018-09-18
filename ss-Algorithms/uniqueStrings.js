7da3a06e156f3d636493534f4f85d2eb3934003d
Jannaee <jannaee@gmail.com>
2018-09-18 16:42:22 -0400

    algorithm testing

--- ss-Algorithms/uniqueStrings.js
+++ ss-Algorithms/uniqueStrings.js
@@ -1,14 +1,17 @@
 //Thinking like a programmer 
 // Go through a string and determine if every character is a unique
-
-
- var car = 'scion';
  
- function stringChecker(string){
     //examine if all characters are unique
-    for (i=0; i<string.length; i++){
-        console.log(string);
+       //if this instance is not a match to the last instance, then return false
+       //however if the first and the last ar not the same then return true and exit out and return to the loop to check the next character
+ function stringChecker(string){
+    for (var i=0; i < string.length; i++){
+       if(string.indexOf(string[i]) !== string.lastIndexOf(string[i])) 
+       console.log('this string is looking unique so far' + string)
+       return false;
     }
+    return true;
  }
-
-stringChecker(car);
+stringChecker('car');
+stringChecker ('smartphone');
+stringChecker ('moon');
