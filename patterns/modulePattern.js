00e3b85c149de91b27a426f4981244699aab6fbf
Jannaee Sick <jannaee@gmail.com>
2018-09-19 11:22:37 -0400

    feat: practice with higher order functions

--- patterns/modulePattern.js
+++ patterns/modulePattern.js
@@ -2,14 +2,35 @@
 //Supports loose coupling,  the best advantage is encapsulation or ability to protect states and behaviors
 //The  module pattern allows for publlic and private as well as protected and privileged access levels
 
-//AN IIFE module - that allows for private scoping
+//AN IIFE module - Immediately invoked function Expression - that allows for private scoping
 
-(function (){
-    //declare private variables and or functions
+//step 1 create the IIFE Module
+// (function (){
+//     //declare private variables and or functions
 
-    return {
-        //declare public variables and/or functions
-    }
+//     return {
+//         //declare public variables and/or functions
+//     }
 
-})();
+// })();
+
+
+//Step 2 instantiate the private variables/or functions
+
+//Step 3 Return the object
+
+//Example provided by tutorial on https://scotch.io/bar-talk/4-javascript-design-patterns-you-should-know
+
+
+
+//Applying what I've learned to my own Module
+
+//A working example of a factory that creates pies
+//create a pie based off of the crust type, the fillings, and add a cost to them
+
+// var pieFactory = (function (){
+//     //private variables
+//     var fillings = ['', '', '',  ]
+
+// })()
 
---
+++ ss-Algorithms/arrayMethods/higherOrderFunctions-map.js
@@ -0,0 +1,13 @@
+//Implement an algorithm to determing if a string has all unique characters
+//Test cases var test1 = 'aaaaa', test2 = 'abbababa', test3 = '.......',
+
+var testString ='aosdnfoian aaaaa andanlskd';
+
+testString.indexOf('aaaaa');
+console.log(testString);//Implement an algorithm to determing if a string has all unique characters
+//Test cases var test1 = 'aaaaa', test2 = 'abbababa', test3 = '.......',
+
+var testString ='aosdnfoian aaaaa andanlskd';
+
+testString.indexOf('aaaaa');
+console.log(testString);
