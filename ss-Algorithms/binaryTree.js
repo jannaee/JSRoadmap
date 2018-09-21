66f0d92fb701bcf1e2a650ecc49c9706fe8752b0
Jannaee Sick <jannaee@gmail.com>
2018-09-20 21:03:53 -0400

    refact: Creating a binary tree program

---
+++ ss-Algorithms/binaryTree.js
@@ -0,0 +1,5 @@
+//Practice in writing classical inheritance
+
+//First id all of the variables that I will need to have access to
+//A tree, the root, left and right sides, nodes, 
+//Actions that may be required moving left and right, or up and down
