8c4738cf17a698cfd4f1400b6f9af68d3eb0e605
Jannaee Sick <jannaee@gmail.com>
2018-10-05 17:13:08 -0400

    refact:HR test

---
+++ hackerRankChallenges/vowelsConsonants.js
@@ -0,0 +1,37 @@
+// function vowelsAndConsonants(s) {
+// Hacker Rank Sample test
+
+
+
+
+//     //print the vowels of the parameter "s"
+//     //print the consonants of the param
+//     //loop through the string and match any items to the exact strings "a" "e" "i" "o" "u"
+//     //print anything that matches, then print everything else.
+//     // const iterator = arraySplit.values();
+
+
+//     const arraySplit = [s.split("")];
+//     const regExArray = s.match(/([aeiou])/g);//[a, e, i, o, u]
+//     let newAdjustedArray = arraySplit.map(function (e){
+//        console.log(regExArray)
+        
+    
+//         return e;
+//     })
+//     console.log(newAdjustedArray)
+
+
+// }
+
+// vowelsAndConsonants('javascriptloops');
+
+
+// //evaluate a string
+// //does the string have any vowels?
+//     //if so return all of them into an array
+//     //if it does not then return string
+// //does the 
+
+
+
--- ss-Algorithms/classes.js
+++ ss-Algorithms/classes.js
@@ -1,8 +1,28 @@
 class Tree {
-    constructor(add, remove, val){
-        this.add = add;
-        this.remove= remove;
-        this.val= val;
+    constructor(add, remove, val){//a special method for create and initialize the tree object
+       console.log(this.add = add);
+        this.remove = remove;
+        this.val = val;
     }
 
+    Add () {//adding a node to the tree
+
+    }
+
+    Remove () {//remove node from the tree
+
+    }
+    
 }//end of Tree
+//new keyword is to create an object
+
+class Bubble {
+    constructor(){
+//do not have to declare variable with normal keywords
+//use this to  
+this.y=100;
+this.x=200
+    }
+}
+
+let bubble = new Bubble();
